def2987032658210f28c2e38ce49847e49b19f74
zivzolti-lab <zivzolti@gmail.com>
2025-11-20 11:53:28 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -3,9 +3,33 @@
 
 def split_before_uppercases(formula):
     pass  # replace the pass with your code
+    start = 0
+    end = 0
+    split_formula = []
+    if not formula:
+      return split_formula
+    else:
+      for char in formula[1:]:
+        if char.isupper():
+          split_formula.append(formula[start:(end+1)])
+          start = end + 1 
+        end += 1
+      split_formula.append(formula[start:(end+1)])
+      return split_formula
 
 def split_at_digit(formula):
     pass  # replace the pass with your code
+    pass # Replace the pass with your code
+    digit_location = 1
+    for char in formula[1:]:
+      if (char.isdigit()):
+        break
+      digit_location += 1
+
+    if digit_location == len(formula):
+      return formula, 1
+    else:
+      return formula[0:digit_location], int(formula[digit_location:])
 
 def count_atoms_in_molecule(molecular_formula):
     """Takes a molecular formula (string) and returns a dictionary of atom counts.  
