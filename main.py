ff34e140e16dbe6b07e023272632b5cef9d80e4b
zivzolti-lab <zivzolti@gmail.com>
2025-11-26 20:19:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 # Add the import statements for functions from string_utils.py and equation_utils.py here
+# Download string_utils.py from your GitHub repository
+!wget https://raw.githubusercontent.com/yotam-biu/ps5/main/string_utils.py -O /content/string_utils.py
+
+# Download equation_utils.py from your GitHub repository
+!wget https://raw.githubusercontent.com/yotam-biu/ps5/main/equation_utils.py -O /content/equation_utils.py
+
+!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
+import lab_setup_do_not_edit
+
+from string_utils.py import parse_chemical_reaction, count_atoms_in_reaction
+from equation_utils.py import build_equations, my_solve
+
 
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
